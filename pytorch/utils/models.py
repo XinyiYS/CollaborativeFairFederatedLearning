655cd1b32e838eaec07eb8b980def044f81697eb
XinyiYS <michaelxysg@gmail.com>
2020-04-14 17:10:32 +0800

    edit the MLP-LR

--- pytorch/utils/models.py
+++ pytorch/utils/models.py
@@ -52,16 +52,15 @@ class LogisticRegression(nn.Module):
 		outputs = self.linear(x)
 		return outputs
 
+
 class MLP_LogReg(nn.Module):
 	def __init__(self, input_dim=86, output_dim=2):
 		super(MLP_LogReg, self).__init__()
-		self.fc1 = nn.Linear(input_dim, 128)
-		self.fc2 = nn.Linear(128, 64)
-		self.fc3 = nn.Linear(64, output_dim)
+		self.fc1 = nn.Linear(input_dim, 64)
+		self.fc2 = nn.Linear(64, output_dim)
 
 		# self.linear = torch.nn.Linear(input_dim, output_dim)
 
 	def forward(self, x):
-		x = torch.tanh(self.fc1(x))
-		x = torch.tanh(self.fc2(x))
-		return self.fc3(x)
+		x = torch.sigmoid(self.fc1(x))
+		return self.fc2(x)
