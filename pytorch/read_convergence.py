cd334d4f9ec9050ea698906b272a18fff1a9c6b7
XinyiYS <michaelxysg@gmail.com>
2020-04-14 17:08:28 +0800

    update the reading scripts

--- pytorch/read_convergence.py
+++ pytorch/read_convergence.py
@@ -3,41 +3,43 @@ import json
 import pandas as pd
 
 dirname = 'logs'
-experimental_combinations = ['p5_e5-20-5_b16_size5000',
-							 'p10_e5-20-5_b16_size10000',
-							 'p20_e5-20-5_b16_size15000']
-
+experimental_combinations = ['p5_e10-50-5_b16_size5000',
+                             'p10_e10-50-5_b16_size10000',
+                             'p20_e10-50-5_b16_size15000'
+                             'p5_e10-100-5_b16_size5000',
+                             'p10_e10-100-5_b16_size10000',
+                             # 'p20_e10-100-5_b16_size15000'
+                             ]
 
 import ast
 import numpy as np
 dfs = []
 
-for combi in experimental_combinations:
-
-	n_workers = int(combi.split('_')[0][1:])
-	fl_epochs = int(combi.split('-')[1])
+# for combi in experimental_combinations:
+for folder in os.listdir(dirname):
+	n_workers = int(folder.split('_')[1][1:])
+	fl_epochs = int(folder.split('-')[1])
 	columns = ['Worker_' + str(i + 1) for i in range(n_workers)]
+
 	experiments = []
 
-	for folder in os.listdir(dirname):
-		if combi in folder:
-			with open(os.path.join(dirname, folder, 'log'), 'r') as log:
-				loginfo = log.readlines()
-			worker_fed_accs_lines = [line.replace('Workers federated accuracies:  ', '') for line in loginfo if 'Workers federated accuracies' in line]
+	with open(os.path.join(dirname, folder, 'log'), 'r') as log:
+		loginfo = log.readlines()
+	worker_fed_accs_lines = [line.replace('Workers federated accuracies:  ', '') for line in loginfo if 'Workers federated accuracies' in line]
 
+	data_rows = []
+	epoch_count = 0
+	for line in worker_fed_accs_lines:
+		line = ast.literal_eval(line)
+		data_row = [ast.literal_eval(acc[:-1]) for acc in line]
+		data_rows.append(data_row)
+
+		epoch_count +=1
+		if epoch_count == fl_epochs:
+
+			experiments.append(np.array(data_rows))
 			data_rows = []
 			epoch_count = 0
-			for line in worker_fed_accs_lines:
-				line = ast.literal_eval(line)
-				data_row = [ast.literal_eval(acc[:-1]) for acc in line]
-				data_rows.append(data_row)
-
-				epoch_count +=1
-				if epoch_count == fl_epochs:
-
-					experiments.append(np.array(data_rows))
-					data_rows = []
-					epoch_count = 0
 
 	experiments = np.asarray(experiments)
 	average_data = np.mean(experiments, axis=0)
@@ -45,5 +47,5 @@ for combi in experimental_combinations:
 	dfs.append(df)
 
 
-print(dfs[2].describe())
-print(dfs[2].to_string())
+print(dfs[1].describe())
+print(dfs[1].to_string())
--- pytorch/read_performance.py
+++ pytorch/read_performance.py
@@ -3,9 +3,13 @@ import json
 import pandas as pd
 
 dirname = 'logs'
-experimental_combinations = ['p5_e5-20-5_b16_size5000',
-                             'p10_e5-20-5_b16_size10000',
-                             'p20_e5-20-5_b16_size15000']
+experimental_combinations = ['p5_e10-50-5_b16_size5000',
+                             'p10_e10-50-5_b16_size10000',
+                             'p20_e10-50-5_b16_size15000'
+                             'p5_e10-100-5_b16_size5000',
+                             'p10_e10-100-5_b16_size10000',
+                             'p20_e10-100-5_b16_size15000'
+                             ]
 
 keys = ['standalone_vs_final_corr_mean',
         'sharingcontribution_vs_final_corr_mean',
@@ -17,17 +21,17 @@ keys = ['standalone_vs_final_corr_mean',
 
 data_rows = []
 for folder in os.listdir(dirname):
-    for combi in experimental_combinations:
-        if combi in folder:
-            # with open(os.path.join(dirname, folder, 'log'), 'r') as log:
-                # loginfo = log.readlines()
-
+        # with open(os.path.join(dirname, folder, 'log'), 'r') as log:
+            # loginfo = log.readlines()
+        try:
             with open(os.path.join(dirname, folder, 'aggregate_dict.txt')) as dict_log:
                 aggregate_dict = json.loads(dict_log.read())
 
             data_row = [folder] + [aggregate_dict[key] for key in keys]
             data_rows.append(data_row)
-            break
+        except:
+            pass
+
 
 df = pd.DataFrame(data_rows, columns=['setup'] + keys)
 print(df.to_string())
