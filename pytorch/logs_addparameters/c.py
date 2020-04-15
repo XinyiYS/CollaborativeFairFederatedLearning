c86c4a6070194993ebba9ae1800374f126e802a0
XinyiYS <michaelxysg@gmail.com>
2020-04-15 13:02:01 +0800

    move logs to unused logs

--- pytorch/logs_addparameters/c.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-# a = 'experiment_p10_e5-10-5_b16_size10000_lr0'
-# print(a.replace('experiment', 'powerlaw'))
-for folder in os.listdir():
-	
-	os.rename(folder, folder.replace('experiment', 'powerlaw'))
