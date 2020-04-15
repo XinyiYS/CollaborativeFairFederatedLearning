3fdc14c3b5350689d4c199c3ae449b5a47b565a1
XinyiYS <michaelxysg@gmail.com>
2020-04-15 10:40:43 +0800

    add in plot and tabulation

---
+++ pytorch/plot.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+fmt_styles = ['', '--','-.', 'o-', 'v-',
+			 '^-', '<','>', '1-','2-',
+			 '3-','4-','s-','p-','*-',
+			 'h-', 'H-', '+-', 'X-','D-']
+def plot(df, save_dir=None,show=False):
+	# Data
+	index = np.arange(1, len(df)+1)
+
+	for column, fmt in zip(df.columns, fmt_styles):
+		plt.plot(index, column, fmt, data=df, label=column, )
+
+	plt.legend()
+	plt.xlabel("Communication Rounds",  fontsize='large', fontweight='bold')
+	plt.ylabel("Test Accuracy",  fontsize='large', fontweight='bold')
+	plt.title('Adult')
+	
+	if save_dir:
+		plt.savefig(save_dir)
+		plt.clf()
+	if show:
+		plt.show()
+	return
+
+
+
--- pytorch/read_performance.py
+++ pytorch/read_performance.py
@@ -11,27 +11,46 @@ experimental_combinations = ['p5_e10-50-5_b16_size5000',
                              'p20_e10-100-5_b16_size15000'
                              ]
 
-keys = ['standalone_vs_final_corr_mean',
-        'sharingcontribution_vs_final_corr_mean',
+fairness_keys = [
         'standalone_vs_federated_perturbed_corr_mean',
+        'standalone_vs_final_corr_mean',
+        'sharingcontribution_vs_final_corr_mean', ]
+
+performance_keys = [
         'federated_final_performance_mean',
-        'CFFL_best_worker_mean',
         'standalone_best_worker_mean',
+        'CFFL_best_worker_mean',
         ]
 
-data_rows = []
+fairness_rows = []
+performance_rows = []
 for folder in os.listdir(dirname):
         # with open(os.path.join(dirname, folder, 'log'), 'r') as log:
             # loginfo = log.readlines()
+        
+        n_workers = int(folder.split('_')[1][1:])
+        fl_epochs = int(folder.split('-')[1])
+        if fl_epochs != 100:
+            continue
         try:
             with open(os.path.join(dirname, folder, 'aggregate_dict.txt')) as dict_log:
                 aggregate_dict = json.loads(dict_log.read())
+            f_data_row = ['P' + str(n_workers)] + [aggregate_dict[f_key][0] for f_key in fairness_keys]
+            p_data_row = ['P' + str(n_workers)] + [aggregate_dict[p_key] * 100 for p_key in performance_keys]
 
-            data_row = [folder] + [aggregate_dict[key] for key in keys]
-            data_rows.append(data_row)
+            fairness_rows.append(f_data_row)
+            performance_rows.append(p_data_row)
         except:
             pass
+            
 
 
-df = pd.DataFrame(data_rows, columns=['setup'] + keys)
+shorthand_f_keys = ['Distriubted', 'CFFL' ,'contri_v_final' ]
+df = pd.DataFrame(fairness_rows, columns=[' '] + shorthand_f_keys).set_index(' ')
 print(df.to_string())
+
+shorthand_p_keys = ['Distributed', 'Standalone', 'CFFL']
+pd.options.display.float_format = '{:,.2f}'.format
+df = pd.DataFrame(performance_rows, columns=[' '] + shorthand_p_keys).set_index(' ').T
+print(df.to_string())
+
