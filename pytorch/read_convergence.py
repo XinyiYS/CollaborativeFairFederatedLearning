934e02c71acf4e36a270626951990e8d155b0412
XinyiYS <michaelxysg@gmail.com>
2020-04-15 10:40:43 +0800

    add in plot and tabulation

--- pytorch/read_convergence.py
+++ pytorch/read_convergence.py
@@ -3,23 +3,38 @@ import json
 import pandas as pd
 
 dirname = 'logs'
-experimental_combinations = ['p5_e10-50-5_b16_size5000',
-                             'p10_e10-50-5_b16_size10000',
-                             'p20_e10-50-5_b16_size15000'
-                             'p5_e10-100-5_b16_size5000',
-                             'p10_e10-100-5_b16_size10000',
-                             # 'p20_e10-100-5_b16_size15000'
-                             ]
+
+
+def parse(folder):
+	setup = {}
+	param = folder.split('_')
+	setup['split'] = param[0]
+	setup['P'] = int(param[1][1:])
+	setup['pretrain_epochs'] =int(param[2].split('-')[0][1:])
+	setup['Communication Rounds'] = int(param[2].split('-')[1])
+	setup['E'] = int(param[2].split('-')[2])
+	setup['B'] = int(param[3][1:])
+	setup['size'] = int(param[4][4:])
+	setup['lr'] = float(param[5][2:])
+	return setup
+
 
 import ast
 import numpy as np
-dfs = []
 
-# for combi in experimental_combinations:
+from plot import plot
+
+dfs = []
+setups = []
+experiment_results = []
+
 for folder in os.listdir(dirname):
 	n_workers = int(folder.split('_')[1][1:])
 	fl_epochs = int(folder.split('-')[1])
-	columns = ['Worker_' + str(i + 1) for i in range(n_workers)]
+	if fl_epochs != 100:
+		continue
+
+	columns = ['party' + str(i + 1) for i in range(n_workers)]
 
 	experiments = []
 
@@ -44,8 +59,19 @@ for folder in os.listdir(dirname):
 	experiments = np.asarray(experiments)
 	average_data = np.mean(experiments, axis=0)
 	df = pd.DataFrame(average_data, columns = columns)
+
+
 	dfs.append(df)
 
+	setup = parse(folder)
+	setups.append(setup)
 
-print(dfs[1].describe())
-print(dfs[1].to_string())
+	experiment_results.append((setup, df))
+
+	figure_dir = os.path.join(dirname, folder, 'figure.png')
+	if os.path.exists(figure_dir):
+		os.remove(figure_dir)
+	if not os.path.exists(figure_dir):
+		plot(df,figure_dir)
+		# exit()
+
