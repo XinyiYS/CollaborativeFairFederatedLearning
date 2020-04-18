0cda1430898dd82d0cf1d58ee885e43a2f381dbe
XinyiYS <michaelxysg@gmail.com>
2020-04-18 18:12:02 +0800

    clean up plot functions and support adult and mnist

--- pytorch/plot.py
+++ pytorch/plot.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import pandas as pd
 
 fmt_styles = ['', '--','-.', 'o-', 'v-',
@@ -8,27 +7,43 @@ fmt_styles = ['', '--','-.', 'o-', 'v-',
 			 '3-','4-','s-','p-','*-',
 			 'h-', 'H-', '+-', 'X-','D-']
 
-FONTSIZE = 16
-def plot(df, save_dir=None,show=False, standalone=False):
+FONTSIZE = 17
+
+Xlabel_NAMES = ['Epochs', 'Communication Rounds', 'Epochs (Communication Rounds)']
+def plot(df, save_dir=None, name='Adult', plot_type=1, show=False):
 	# Data
 	index = np.arange(1, len(df)+1)
 
 	for column, fmt in zip(df.columns, fmt_styles):
 		plt.plot(index, column, fmt, data=df, label=column, )
 
-	plt.legend(loc='lower right')
-	plt.xlabel("Communication Rounds",  fontsize=FONTSIZE, fontweight='bold')
-	if standalone:
-		plt.xlabel("Epochs",  fontsize=FONTSIZE, fontweight='bold')
+	if len(df.columns) > 10:
+		plt.legend(loc='lower right',fontsize=8.2)
+	else:
+		plt.legend(loc='lower right')
 
+	plt.xlabel(Xlabel_NAMES[plot_type],  fontsize=FONTSIZE, fontweight='bold')
 	plt.ylabel("Test Accuracy",  fontsize=FONTSIZE, fontweight='bold')
-	locs, labels = plt.yticks()
-	n_ticks = len(locs)
-	y_ticks = np.linspace(locs[0], locs[-1], n_ticks)
-	plt.yticks(np.round(np.array(locs), 2) ,fontsize=14)
+	
 
+	# reformat the yticks
+	if name =='Adult':
+		locs, labels = plt.yticks()
+		n_ticks = len(locs)
+		y_ticks = np.linspace(locs[0], locs[-1], n_ticks)
+		plt.yticks(np.round(np.array(locs), 2) ,fontsize=14)
+		# plt.ylim(0.6, 1.0)
+		# plt.yticks(np.arange(0.6, 0.90, step=0.05), size=14)
+	else: # for MNIST
+		locs, labels = plt.yticks()
+		n_ticks = len(locs)
+		y_ticks = np.linspace(locs[0], locs[-1], n_ticks)
+		plt.yticks(np.round(np.array(locs), 2) ,fontsize=14)
+		# plt.ylim(0.6, 1.02)
+		# plt.yticks(np.arange(0.6, 1.02, step=0.05), size=14)
 
-	plt.title('Adult', fontsize=FONTSIZE)
+	plt.title(name, fontsize=FONTSIZE)
+	plt.tight_layout()
 
 	if save_dir:
 		plt.savefig(save_dir)
@@ -38,25 +53,35 @@ def plot(df, save_dir=None,show=False, standalone=False):
 	return
 
 
-def plot_one(df, save_dir=None, show=False):
+
+'''
+def plot_one(df, save_dir=None, show=False, name='Adult'):
 	# Data
 	index = np.arange(1, len(df)+1)
 	fmt_styles = ['ro-', 'c.-', 'm<-']
 	for column, fmt in zip(df.columns, ['ro-', 'c.-', 'm<-']):
 		plt.plot(index, column, fmt, data=df, label=column, )
 
-	plt.legend(loc='lower right')
+	if len(df.columns) > 10:
+		plt.legend(loc='lower right',fontsize=8.5)
+	else:
+		plt.legend(loc='lower right')	
 	plt.xlabel("Epochs (Communication Rounds)",  fontsize=FONTSIZE, fontweight='bold')
 	plt.ylabel("Test Accuracy",  fontsize=FONTSIZE, fontweight='bold')
-	
+
 	# reformat the yticks
 	locs, labels = plt.yticks()
 	n_ticks = len(locs)
 	y_ticks = np.linspace(locs[0], locs[-1], n_ticks)
 	plt.yticks(np.round(np.array(locs), 2) ,fontsize=14)
+	
+	# fix a constant size yticks
+	plt.yticks(np.arange(0.6, 1.02, step=0.05),size=14)
 
 
-	plt.title('Adult', fontsize=FONTSIZE)
+	plt.title(name, fontsize=FONTSIZE)
+	plt.tight_layout()
+
 	
 	if save_dir:
 		plt.savefig(save_dir)
@@ -64,3 +89,4 @@ def plot_one(df, save_dir=None, show=False):
 	if show:
 		plt.show()
 	return
+'''
--- pytorch/read_convergence.py
+++ pytorch/read_convergence.py
@@ -4,63 +4,200 @@ import pandas as pd
 import ast
 import numpy as np
 
-from plot import plot, plot_one
+from plot import plot
 
 
 def parse(folder):
-	setup = {}
-	param = folder.split('_')
-	setup['split'] = param[0]
-	setup['P'] = int(param[1][1:])
-	setup['pretrain_epochs'] = int(param[2].split('-')[0][1:])
-	setup['Communication Rounds'] = int(param[2].split('-')[1])
-	setup['theta'] = float(param[6].replace('theta', ''))
-	setup['E'] = int(param[2].split('-')[2])
-	setup['B'] = int(param[3][1:])
-	setup['size'] = int(param[4][4:])
-	setup['lr'] = float(param[5][2:])
-	return setup
+    setup = {}
+    param = folder.split('_')
+
+    setup['name'] = 'Adult'
+    if 'MLP_Net' in folder or 'CNN_Net' in folder or 'classimbalance' in folder:
+        setup['name'] = 'mnist'
+    setup['model'] = param[-1]
+    if setup['model'] == 'LogisticRegression':
+        setup['model'] = 'LR'
+
+    setup['split'] = param[0]
+    setup['P'] = int(param[1][1:])
+    setup['pretrain_epochs'] = int(param[2].split('-')[0][1:])
+    setup['Communication Rounds'] = int(param[2].split('-')[1])
+    setup['theta'] = float(param[6].replace('theta', ''))
+    setup['E'] = int(param[2].split('-')[2])
+    setup['B'] = int(param[3][1:])
+    setup['size'] = int(param[4][4:])
+    setup['lr'] = float(param[5][2:])
+    return setup
 
 
 def get_performance(accs_lines, n_runs, fl_epochs):
+    """
+    Arguments: in the accs_lines read from the log file, could be standalone, cffl, dssgd
 
-	final_accs_lines = [ accs_lines[ (i+1) * (fl_epochs+1) -1]     for  i in range(n_runs)  ]
+    Returns the performance over the fl_epochs, averaged over n_runs
+    """
 
-	data_rows = []
-	for line in final_accs_lines:
-		data_row = [ast.literal_eval(acc[:-1]) for acc in ast.literal_eval(line)]
-		data_rows.append(data_row)
-	data_rows = np.asarray(data_rows)
-	performance = np.mean(data_rows, axis=0).round(3)
-	# workerId = np.mean(data_rows, axis=0).argmax()
-	return performance
+    final_accs_lines = [accs_lines[
+        (i + 1) * (fl_epochs + 1) - 1] for i in range(n_runs)]
+
+    data_rows = []
+    for line in final_accs_lines:
+        data_row = [ast.literal_eval(acc[:-1])
+                    for acc in ast.literal_eval(line)]
+        data_rows.append(data_row)
+    data_rows = np.asarray(data_rows)
+    performance = np.mean(data_rows, axis=0).round(3)
+    # workerId = np.mean(data_rows, axis=0).argmax()
+    return performance
+
+
+def get_averaged_results(accs_lines, fl_epochs):
+    data_rows = []
+    experiments = []
+    epoch_count = 0
+    for line in accs_lines:
+        line = ast.literal_eval(line)
+        data_row = [ast.literal_eval(acc[:-1]) for acc in line]
+        data_rows.append(data_row)
+
+        epoch_count += 1
+        if epoch_count == fl_epochs + 1:
+            # read extra row to skip
+            data_rows = data_rows[:-1]
+
+            experiments.append(np.array(data_rows))
+            data_rows = []
+            epoch_count = 0
+
+    experiments = np.asarray(experiments)
+    average_data = np.mean(experiments, axis=0)
+    average_data /= 100.0  # make it percentage
+    return average_data
+
+
+def get_averaged_results_by_workerId(zipped_accs_lines, fl_epochs, workerId):
+    data_rows = []
+    epoch_count = 0
+    experiments = []
+    for lines in zipped_accs_lines:
+        data_row = [ast.literal_eval(ast.literal_eval(
+            line)[workerId][:-1]) for line in lines]
+        data_rows.append(data_row)
+
+        epoch_count += 1
+        if epoch_count == fl_epochs + 1:
+            # skip one last row from performance analysis
+            experiments.append(np.array(data_rows[:-1]))
+            data_rows = []
+            epoch_count = 0
+    return np.asarray(experiments).mean(axis=0) / 100.0
 
 
 def get_best_cffl_worker(dirname, folder):
-	# need to figure out which is the best worker
-	fl_epochs = int(folder.split('-')[1])
-	n_runs = int(folder[folder.find('runs')-1])
+    """
+    Arguments: dirname: directory the folder is in, to find the folder
+                    folder: the folder that contains the experiment
 
-	with open(os.path.join(dirname, folder, 'log'), 'r') as log:
-		loginfo = log.readlines()
+    Returns: the best cffl worker among the n_runs of the experiments, determined through the average test accs
+                    <optional> performance_dict: averaged performance over n_runs, for all workers, for three types of models
+    """
 
-	worker_cffl_accs_lines = [line.replace('Workers CFFL      accuracies:  ', '')
-										   for line in loginfo if 'Workers CFFL      accuracies' in line]
-	worker_standalone_accs_lines = [line.replace(
-		'Workers standlone accuracies:  ', '') for line in loginfo if 'Workers standlone accuracies:  ' in line]
-	worker_dssgd_accs_lines = [line.replace('Workers DSSGD     accuracies:  ', '')
-												for line in loginfo if 'Workers DSSGD     accuracies:  ' in line]
+    # need to figure out which is the best worker
+    fl_epochs = int(folder.split('-')[1])
+    n_runs = int(folder[folder.find('runs') - 1])
 
-	cffl_performances  = get_performance(worker_cffl_accs_lines, n_runs, fl_epochs)
-	dssgd_performances = get_performance(worker_dssgd_accs_lines, n_runs, fl_epochs)
-	standalone_performances = get_performance(worker_standalone_accs_lines, n_runs, fl_epochs)
+    with open(os.path.join(dirname, folder, 'log'), 'r') as log:
+        loginfo = log.readlines()
 
-	workerId = cffl_performances.argmax()
-	performance_dict = {'cffl': cffl_performances, 'dssgd':dssgd_performances, 'standalone':standalone_performances}
+    worker_cffl_accs_lines = [line.replace('Workers CFFL      accuracies:  ', '')
+                              for line in loginfo if 'Workers CFFL      accuracies' in line]
+    worker_standalone_accs_lines = [line.replace(
+        'Workers standlone accuracies:  ', '') for line in loginfo if 'Workers standlone accuracies:  ' in line]
+    worker_dssgd_accs_lines = [line.replace('Workers DSSGD     accuracies:  ', '')
+                               for line in loginfo if 'Workers DSSGD     accuracies:  ' in line]
 
-	return workerId, performance_dict
+    cffl_performances = get_performance(
+        worker_cffl_accs_lines, n_runs, fl_epochs)
+    dssgd_performances = get_performance(
+        worker_dssgd_accs_lines, n_runs, fl_epochs)
+    standalone_performances = get_performance(
+        worker_standalone_accs_lines, n_runs, fl_epochs)
+
+    workerId = cffl_performances.argmax()
+    performance_dict = {'cffl': cffl_performances,
+                        'dssgd': dssgd_performances, 'standalone': standalone_performances}
+
+    return workerId, performance_dict
 
 
+def plot_convergence(dirname):
+    dfs = []
+    setups = []
+    experiment_results = []
+
+    for folder in os.listdir(dirname):
+        if os.path.isfile(os.path.join(dirname, folder)) or not 'complete.txt' in os.listdir(os.path.join(dirname, folder)):
+            continue
+
+        n_workers = int(folder.split('_')[1][1:])
+        fl_epochs = int(folder.split('-')[1])
+        setup = parse(folder)
+
+        columns = ['party' + str(i + 1) for i in range(n_workers)]
+
+        with open(os.path.join(dirname, folder, 'log'), 'r') as log:
+            loginfo = log.readlines()
+
+        worker_cffl_accs_lines = [line.replace(
+            'Workers CFFL      accuracies:  ', '') for line in loginfo if 'Workers CFFL      accuracies' in line]
+        worker_standalone_accs_lines = [line.replace(
+            'Workers standlone accuracies:  ', '') for line in loginfo if 'Workers standlone accuracies:  ' in line]
+        worker_dssgd_accs_lines = [line.replace('Workers DSSGD     accuracies:  ', '')
+                                   for line in loginfo if 'Workers DSSGD     accuracies:  ' in line]
+
+        averaged_cffl_accs = get_averaged_results(
+            worker_cffl_accs_lines, fl_epochs)
+        averaged_standalone_accs = get_averaged_results(
+            worker_standalone_accs_lines, fl_epochs)
+
+        cffl_df = pd.DataFrame(averaged_cffl_accs, columns=columns)
+        standalone_df = pd.DataFrame(averaged_standalone_accs, columns=columns)
+
+        cffl_figure_dir = os.path.join(dirname, folder, 'figure.png')
+        standlone_figure_dir = os.path.join(dirname, folder, 'standlone.png')
+
+
+        best_workerId, _ = get_best_cffl_worker(dirname, folder)
+
+        averaged_worker_accs = get_averaged_results_by_workerId(zip(
+            worker_standalone_accs_lines, worker_dssgd_accs_lines, worker_cffl_accs_lines), fl_epochs, best_workerId)
+        worker_df = pd.DataFrame(averaged_worker_accs, columns=[
+                                 'Standlone', 'Distributed', 'CFFL'])
+        worker_figure_dir = os.path.join(
+            dirname, folder, 'convergence_for_one.png')
+
+        if os.path.exists(cffl_figure_dir):
+            os.remove(cffl_figure_dir)
+        plot(cffl_df, cffl_figure_dir, name=setup['name'], plot_type=0)
+
+        if os.path.exists(standlone_figure_dir):
+            os.remove(standlone_figure_dir)
+        plot(standalone_df, standlone_figure_dir, name=setup['name'], plot_type=1)
+
+        if os.path.exists(worker_figure_dir):
+            os.remove(worker_figure_dir)
+        plot(worker_df, worker_figure_dir, name=setup['name'], plot_type=2)
+
+    return
+
+
+if __name__ == '__main__':
+    dirname = 'logs'
+    experiment_results = plot_convergence(dirname)
+    # plot_convergence_for_one(dirname, mode='best', workerId=-1)
+
+
+'''
 def plot_convergence_for_one(dirname, mode='best', workerId=-1):
 
 	for folder in os.listdir(dirname):
@@ -84,7 +221,6 @@ def plot_convergence_for_one(dirname, mode='best', workerId=-1):
 			'Workers standlone accuracies:  ', '') for line in loginfo if 'Workers standlone accuracies:  ' in line]
 		worker_dssgd_accs_lines = [line.replace('Workers DSSGD     accuracies:  ', '')
 												for line in loginfo if 'Workers DSSGD     accuracies:  ' in line]
-
 
 		data_rows = []
 		epoch_count = 0
@@ -113,86 +249,7 @@ def plot_convergence_for_one(dirname, mode='best', workerId=-1):
 		if os.path.exists(figure_dir):
 			os.remove(figure_dir)
 		if not os.path.exists(figure_dir):
-			plot_one(df,figure_dir)
+			plot(df, figure_dir, standalone=True)
+
 	return
-
-def plot_convergence(dirname):
-	dfs = []
-	setups = []
-	experiment_results = []
-
-	for folder in os.listdir(dirname):
-		if os.path.isfile(os.path.join(dirname, folder)) or not 'complete.txt' in os.listdir(os.path.join(dirname, folder)):
-			continue
-
-		n_workers = int(folder.split('_')[1][1:])
-		fl_epochs = int(folder.split('-')[1])
-
-		columns = ['party' + str(i + 1) for i in range(n_workers)]
-		
-		experiments = []
-		standlone_experiments = []
-		with open(os.path.join(dirname, folder, 'log'), 'r') as log:
-			loginfo = log.readlines()
-		worker_cffl_accs_lines = [line.replace('Workers CFFL      accuracies:  ', '') for line in loginfo if 'Workers CFFL      accuracies' in line]
-		
-		worker_standalone_accs_lines = [line.replace('Workers standlone accuracies:  ', '') for line in loginfo if 'Workers standlone accuracies:  ' in line]
-		# worker_dssgd_accs_lines = [line.replace('Workers DSSGD     accuracies:  ', '') for line in loginfo if 'Workers DSSGD     accuracies:  ' in line]
-
-		data_rows = []
-		standlone_data_rows = []
-		epoch_count = 0
-		for line, standlone_line in zip(worker_cffl_accs_lines, worker_standalone_accs_lines):
-
-			line = ast.literal_eval(line)
-			data_row = [ast.literal_eval(acc[:-1]) for acc in line]
-			data_rows.append(data_row)
-
-			standlone_line = ast.literal_eval(standlone_line)
-			standlone_data_row = [ast.literal_eval(acc[:-1]) for acc in standlone_line]
-			standlone_data_rows.append(standlone_data_row)
-
-			epoch_count += 1
-			if epoch_count == fl_epochs + 1:
-				# read extra row to skip
-
-				data_rows = data_rows[:-1]
-
-				standlone_data_rows = standlone_data_rows[:-1]
-
-
-				experiments.append(np.array(data_rows))
-				standlone_experiments.append(np.array(standlone_data_rows))
-				data_rows = []
-				standlone_data_rows = []
-				epoch_count = 0
-
-		experiments = np.asarray(experiments)
-		average_data = np.mean(experiments, axis=0)
-		average_data /= 100.0 # make it percentage
-		df = pd.DataFrame(average_data, columns = columns)
-		dfs.append(df)
-
-		setup = parse(folder)
-		setups.append(setup)
-		experiment_results.append((setup, df))
-
-		standlone_experiments = np.asarray(standlone_experiments)
-		standalone_average_data = np.mean(standlone_experiments, axis=0)
-		standalone_average_data /= 100.0 # make it percentage
-		standalone_df = pd.DataFrame(standalone_average_data, columns = columns)
-
-		figure_dir = os.path.join(dirname, folder, 'figure.png')
-		standlone_figure_dir = os.path.join(dirname, folder, 'standlone.png')
-		if os.path.exists(figure_dir):
-			os.remove(figure_dir)
-		if not os.path.exists(figure_dir):
-			plot(df,figure_dir)
-			plot(standalone_df, standlone_figure_dir, standalone=True)
-
-	return experiment_results
-
-if __name__ == '__main__':
-	dirname = 'logs'
-	experiment_results = plot_convergence(dirname)
-	plot_convergence_for_one(dirname, mode='best', workerId=-1)
+'''
