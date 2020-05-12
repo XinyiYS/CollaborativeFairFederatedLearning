9eb901ba0f2000ee27195ced868918eeb7866f7a
XinyiYS <michaelxysg@gmail.com>
2020-05-13 00:18:00 +0800

    missed a file

--- pytorch/arguments.py
+++ pytorch/arguments.py
@@ -102,7 +102,7 @@ names_args = {
 	'pretraining_lr' : 0.1, # only used during pretraining for CFFL models, no decay
 	'dssgd_lr': 0.001, # used for dssgd model, no decay
 	'lr': 0.005,
-	'grad_clip':0.001,
+	'grad_clip':0.1,
 	'gamma':0.955,   #0.955**100 ~= 0.01
 
 	# training parameters
@@ -140,15 +140,58 @@ sst_args = {
 	'kernel_sizes': [3,4,5],
 
 	'optimizer_fn': optim.Adam,
-	'loss_fn': nn.NLLLoss(), 
+	'loss_fn': nn.CrossEntropyLoss(), 
 	'pretraining_lr' : 0.1, # only used during pretraining for CFFL models, no decay
 	'dssgd_lr': 0.001, # used for dssgd model, no decay
 	'lr': 0.001,
-	'grad_clip':0.001,
+	'grad_clip':0.1,
 	'gamma':0.955,   #0.955**100 ~= 0.01
 
 	# training parameters
 	'pretrain_epochs': 0,
+	'fl_epochs': 100,
+	'fl_individual_epochs': 5,
+	'aggregate_mode':'sum',  # 'mean', 'sum', credit-sum
+}
+
+
+
+mr_args = {
+	# system parameters
+	'gpu': 0,
+	'device': torch.device("cuda" if torch.cuda.is_available() and use_cuda else "cpu"),
+	'save_gpu': True,
+	# setting parameters
+	'dataset': 'mr',
+	'sample_size_cap': 5000,
+	'n_workers': 5,
+	'split': 'powerlaw', #or 'powerlaw' classimbalance
+	'theta': 0.1,  # privacy level -> at most (theta * num_of_parameters) updates
+	'batch_size' : 64, 
+	'train_val_split_ratio': 0.9,
+	'alpha': 5,
+	'epoch_sample_size':10,
+	'n_freeriders': 0,
+
+
+	# model parameters
+	'model_fn': CNN_Text,
+	'embed_num': 20000,
+	'embed_dim': 300,
+	'class_num': 2,
+	'kernel_num': 100,
+	'kernel_sizes': [3,4,5],
+
+	'optimizer_fn': optim.Adam,
+	'loss_fn': nn.NLLLoss(), 
+	'pretraining_lr' : 0.1, # only used during pretraining for CFFL models, no decay
+	'dssgd_lr': 0.001, # used for dssgd model, no decay
+	'lr': 0.001,
+	'grad_clip':0.1,
+	'gamma':0.955,   #0.955**100 ~= 0.01
+
+	# training parameters
+	'pretrain_epochs': 5,
 	'fl_epochs': 100,
 	'fl_individual_epochs': 5,
 	'aggregate_mode':'sum',  # 'mean', 'sum', credit-sum
