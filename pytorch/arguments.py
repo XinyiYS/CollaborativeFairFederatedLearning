fix the adult csv processing mistakes created new adult.csv created corresponding [train, test]-[data, labels] for lua fixed the logic for load_adult.py used by lua: removing extra normalisation fixed the models (MLP and LogReg) used for adult to have default input 86 (previously 85) to accommodate the newly processed csv Switch to use MLP for adult seems to have better fairness ~0.85 over LogReg ~0.04

pytorch/arguments.py
@@ -34,9 +34,9 @@ adult_args = {
 	'n_freeriders': 0,
 
 	# model parameters
-	'model_fn': LogisticRegression,
+	'model_fn': MLP,
 	'optimizer_fn': optim.SGD,
-	'loss_fn': nn.CrossEntropyLoss(),  #CrossEntropyLoss NLLLoss
+	'loss_fn': nn.NLLLoss(),  #CrossEntropyLoss NLLLoss
 	'pretraining_lr' : 5e-3,  # only used during pretraining for CFFL models, no decay
 	'dssgd_lr': 1e-2, # used for dssgd model, no decay	
 	'std_lr': 1e-2,

pytorch/test.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import json
-
+import random
 import numpy as np
 import torch
 
@@ -84,32 +84,33 @@ from arguments import adult_args, mnist_args, names_args, update_gpu, mr_args, s
 def init_deterministic():
 	torch.manual_seed(1234)
 	np.random.seed(1234)
-	torch.backends.cudnn.deterministic = True
-	torch.backends.cudnn.benchmark = False
+	random.seed(1234)
+	# torch.backends.cudnn.deterministic = True
+	# torch.backends.cudnn.benchmark = False
 
 if __name__ == '__main__':
 	# init steps
 	init_deterministic()
 
+
 	args = adult_args
 	args['theta'] = 0.1
-	args['n_workers'] = 5
-	args['sample_size_cap'] = 4000
-	args['fl_epochs'] = 20
-	args['fl_individual_epochs'] = 5
+	# args['n_workers'] = 5
+	# args['sample_size_cap'] = 4000
 	# args = cifar_cnn_args
-	# args['n_workers'] = 20
-	# args['sample_size_cap'] = 40000
-	# args['theta'] = 0.1 
 	# args['batch_size'] = 128
-	args['gamma'] = 0.977
+	# args['gamma'] = 1
+	# args['optimizer_fn'] = optim.SGD
+	# args['lr'] = 5e-3
+	# args['fl_epochs'] = 100
+	# args['dssgd_lr'] = 5e-3
 	# args['split']='powerlaw'
 	# args['grad_clip']=0.001
 	# args['pretrain_epochs']=5
 	# args['pretraining_lr']=0.005
 	# args['dssgd_lr']=0.005
 	args['aggregate_mode']='sum'
-
+	# args['n_freeriders'] = 1
 	run_experiments(args, 1)
 	
 	exit()

pytorch/utils/models.py
@@ -44,7 +44,7 @@ class MLP_Net(nn.Module):
 
 class LogisticRegression(nn.Module):
 
-	def __init__(self, input_dim=85, output_dim=2, device=None):
+	def __init__(self, input_dim=86, output_dim=2, device=None):
 		super(LogisticRegression, self).__init__()
 		self.input_dim = input_dim
 		self.output_dim = output_dim
@@ -57,7 +57,7 @@ class LogisticRegression(nn.Module):
 
 class MLP(nn.Module):
 
-	def __init__(self, input_dim=85, output_dim=2, device=None):
+	def __init__(self, input_dim=86, output_dim=2, device=None):
 		super(MLP, self).__init__()
 		self.fc1 = nn.Linear(input_dim, 32)
 		self.fc2 = nn.Linear(32, output_dim)